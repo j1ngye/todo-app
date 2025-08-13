6cddc384be6f3c18a672ebaef4fb102d137e166f
j1ngye <216147947+j1ngye@users.noreply.github.com>
2025-08-13 19:49:22 +0800

    Create project and todo classes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./styles.css";
+
+class Project {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  addTodo(todo) {
+    this.todos.push(todo);
+  }
+}
+
+class Todo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
