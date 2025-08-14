bd01cdd450d688782af8dfd211e96c37b61511e5
j1ngye <216147947+j1ngye@users.noreply.github.com>
2025-08-14 14:40:05 +0800

    Create displayNewProject function and add it into projectFormSubmitButton click event

--- src/index.js
+++ src/index.js
@@ -1,5 +1,61 @@
 import "./styles.css";
 
+const projectContainer = document.querySelector(".project-container");
+const addProjectButton = document.querySelector(
+  ".project-container .add-button"
+);
+const addProjectForm = document.querySelector(".add-project-form");
+const projectFormSubmitButton = document.querySelector(
+  ".add-project-form .submit-button"
+);
+
+let projects = [];
+function createProject(title) {
+  projects.push(new Project(title));
+}
+
+function displayNewProject(project) {
+  const projectDiv = document.createElement("div");
+  const projectTitle = document.createElement("p");
+  const DeleteButton = document.createElement("button");
+
+  projectDiv.classList.add("project");
+  projectTitle.classList.add("title");
+  DeleteButton.classList.add("delete-button");
+
+  projectTitle.textContent = project.title;
+  DeleteButton.textContent = "X";
+
+  DeleteButton.addEventListener("click", () => projectDiv.remove());
+
+  projectDiv.appendChild(projectTitle);
+  projectDiv.appendChild(DeleteButton);
+
+  projectContainer.appendChild(projectDiv);
+}
+
+function createTodo(projectTitle, title, description, dueDate, priority) {
+  for (let p of projects) {
+    if (p.title === projectTitle) {
+      p.addTodo(new Todo(title, description, dueDate, priority));
+    }
+  }
+}
+
+addProjectButton.addEventListener("click", () => {
+  addProjectForm.style.display = "flex";
+});
+
+projectFormSubmitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const inputVal = addProjectForm.children[0].value;
+  createProject(inputVal);
+  displayNewProject(projects[projects.length - 1]);
+  addProjectForm.children[0].value = "";
+  addProjectForm.style.display = "none";
+  console.log(projects);
+});
+
 class Project {
   constructor(title) {
     this.title = title;
@@ -19,35 +75,3 @@ class Todo {
     this.priority = priority;
   }
 }
-
-let projects = [];
-function createProject(title) {
-  projects.push(new Project(title));
-}
-
-function createTodo(projectTitle, title, description, dueDate, priority) {
-  for (let p of projects) {
-    if (p.title === projectTitle) {
-      p.addTodo(new Todo(title, description, dueDate, priority));
-    }
-  }
-}
-
-const addProjectButton = document.querySelector(
-  ".project-container .add-button"
-);
-const addProjectForm = document.querySelector(".add-project-form");
-const projectFormSubmitButton = document.querySelector(
-  ".add-project-form .submit-button"
-);
-
-addProjectButton.addEventListener("click", () => {
-  addProjectForm.style.display = "flex";
-});
-
-projectFormSubmitButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  const inputVal = addProjectForm.children[0].value;
-  createProject(inputVal);
-  addProjectForm.style.display = "none";
-});
